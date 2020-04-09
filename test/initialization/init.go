6c2d048447168a6172fbf1365011c09f0996d8ce
unknown <hansmouse@gmail.com>
2020-04-09 23:01:51 +0900

    ...

---
+++ test/initialization/init.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	constantTest()
+	initTest()
+}
+
+// ByteSize ...
+type ByteSize float64
+
+// Byte size 단위 값 정의
+const (
+	_           = iota // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func (b ByteSize) String() string {
+	switch {
+	case b >= YB:
+		return fmt.Sprintf("%.2fYB", b/YB)
+	case b >= ZB:
+		return fmt.Sprintf("%.2fZB", b/ZB)
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
+
+func constantTest() {
+
+	fmt.Printf("KB=%f, MB=%f, YB=%f\n", KB, MB, YB)
+
+	bytes := ByteSize(123412243234)
+
+	strBytes := bytes.String()
+	fmt.Printf("bytes=%#v, strBytes=%#v\n", bytes, strBytes)
+}
+
+func initTest() {
+
+	//os.Setenv("HOME", "C:\\Users\\Administrator")
+	os.Unsetenv("HOME")
+	os.Setenv("USER", "hanmouse")
+
+	var (
+		home   = os.Getenv("HOME")
+		user   = os.Getenv("USER")
+		gopath = os.Getenv("GOPATH")
+	)
+
+	if user == "" {
+		log.Fatal("$USER not set")
+	}
+
+	if home == "" {
+		home = "/home/" + user
+	}
+
+	if gopath == "" {
+		gopath = home + "/go"
+	}
+
+	// gopath may be overridden by --gopath flag on command line.
+	flag.StringVar(&gopath, "gopath", gopath, "override default GOPATH")
+
+	fmt.Println(gopath)
+}
