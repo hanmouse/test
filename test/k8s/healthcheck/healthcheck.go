bc74f7bc5995737ea0e3b508ee1d26388dd55d1d
hanmouse <hansmouse@gmail.com>
2020-04-13 20:31:33 +0900

    기본 기능만 실행되도록 수정

--- test/k8s/healthcheck/healthcheck.go
+++ test/k8s/healthcheck/healthcheck.go
@@ -1,22 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/heptiolabs/healthcheck"
 )
 
+var testURL = "https://heptio.com"
+
+// TODO
+func dumpRequest(handler healthcheck.Handler, httpMethod string, location string) string {
+	resp, err := http.Get(testURL)
+	if err != nil {
+		return fmt.Sprint(err)
+	}
+
+	defer resp.Body.Close()
+
+	//return fmt.Sprintf("%#v\n", resp.Header)
+	return fmt.Sprintf("Status: %#v\n", resp.Status)
+
+	/*
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Sprint(err)
+		}
+
+		return string(body)
+	*/
+}
+
 // TODO
 func main() {
+
+	//var isServerDone chan bool
+
 	health := healthcheck.NewHandler()
 
-	// Our app is not happy if we've got more than 100 goroutines running.
-	health.AddLivenessCheck("goroutine-threshold", healthcheck.GoroutineCountCheck(100))
+	health.AddLivenessCheck("liveness-check", healthcheck.HTTPGetCheck(testURL, 5*time.Second))
 
-	// Our app is not ready if we can't resolve our upstream dependency in DNS.
-	health.AddReadinessCheck(
-		"upstream-dep-dns",
-		healthcheck.DNSResolveCheck("upstream.example.com", 50*time.Millisecond))
+	go http.ListenAndServe("0.0.0.0:8080", health)
+
+	//<-isServerDone
+
+	fmt.Print(dumpRequest(health, "GET", "/live"))
+
+	/*
+		health.AddReadinessCheck(
+			"upstream-dep-dns",
+			healthcheck.DNSResolveCheck("upstream.example.com", 50*time.Millisecond))
+	*/
 
 	// Our app is not ready if we can't connect to our database (`var db *sql.DB`) in <1s.
 	//db := sql.DB
