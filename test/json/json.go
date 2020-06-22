e2af5d268d9ce34e5596ecca664e19b8a827f8b0
hanmouse <hansmouse@gmail.com>
2020-06-22 16:22:16 +0900

    ...

--- test/json/json.go
+++ test/json/json.go
@@ -5,25 +5,34 @@ import (
 	"fmt"
 )
 
+// JSONMarshalIndentUser 는 JSON marshalling 시 json.Marshal() 대신 json.MarshalIndent()를 사용한다고 지정하기 위한 interface이다.
+type JSONMarshalIndentUser interface {
+	// 특정 type이 JSONMarshalIndentUser interface type이 되려면 해당 type에 대해 아래와 같이 빈 함수를 정의해 주면 된다.
+	// func (r *SomeType) UseJSONMarshalIndent() {}
+	UseJSONMarshalIndent()
+}
+
 // HeadphoneSystem ...
 type HeadphoneSystem struct {
-	Headphones []string
-	DACs       []string
-	AMPs       []string
+	Headphones []string `json:"headphones"`
+	DACs       []string `json:"dacs"`
+	AMPs       []string `json:"amps"`
 }
+
+var _ JSONMarshalIndentUser = (*HeadphoneSystem)(nil)
+
+// UseJSONMarshalIndent ...
+func (r *HeadphoneSystem) UseJSONMarshalIndent() {}
 
 func main() {
 
-	headphoneSystem := HeadphoneSystem{
+	headphoneSystem := &HeadphoneSystem{
 		Headphones: []string{"HD58X", "HD600", "HD6XX"},
 		DACs:       []string{"Modi 3"},
 		AMPs:       []string{"Magni Heresy", "Asgard 3"},
 	}
 
-	encoded, err := json.MarshalIndent(headphoneSystem, "", "    ")
-	if err != nil {
-		panic(err)
-	}
+	encoded, err := marshalJSON(headphoneSystem)
 
 	err = nil
 
@@ -36,4 +45,15 @@ func main() {
 	}
 
 	fmt.Printf("decoded: %#v\n", decoded)
+}
+
+func marshalJSON(data interface{}) (marshalledData []byte, err error) {
+	switch data := data.(type) {
+	case JSONMarshalIndentUser:
+		marshalledData, err = json.MarshalIndent(data, "", "  ")
+	default:
+		marshalledData, err = json.Marshal(data)
+	}
+
+	return marshalledData, err
 }
